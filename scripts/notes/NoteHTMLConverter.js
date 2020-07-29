4032a033a45a22582cd5c9ba95a5203c5e4b0f67
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-07-29 15:50:55 -0500

    fixed timestamp

--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -4,7 +4,7 @@ export const noteHTML = (noteObj) => {
             <div class="input note--title">Title: ${ noteObj.title }</div>
             <div class="input note--content">${ noteObj.content }</div>
             <div class="input note--author">Author: ${ noteObj.author }</div>
-            <div class="input note--timestamp">Timestamp: ${ new Date (noteObj.timestamp).toLocaleDateString('en-us') }</div>
+            <div class="input note--timetamp">Timestamp: ${ new Date(noteObj.timestamp).toLocaleDateString('en-US') }</div>
         </section>
             `
 }
