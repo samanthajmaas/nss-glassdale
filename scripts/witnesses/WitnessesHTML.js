c00a918b8a4e7d6e2608194d4fd172dd68bd677e
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-08-03 15:57:33 -0500

    fixed master hopefully

---
+++ scripts/witnesses/WitnessesHTML.js
@@ -0,0 +1,9 @@
+export const witnessHTML = (witnessObj) => {
+    return `
+    <section class="witnesses">
+        <div class="wtinessName">Name: ${witnessObj.name}</div>
+        <div class="witnessStatement"> Statement: ${witnessObj.statements}</div>
+        <br></br>
+    </section>
+    `
+}
